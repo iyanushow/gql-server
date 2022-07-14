516b9a453fbf0ef1ad8db67f284bfe027397c434
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-07-14 10:55:56 +0100

    fix cors issue

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const cors = require("cors");
 const express = require("express");
-
-const RootSchema = require("./schema/schema");
 const { graphqlHTTP } = require("express-graphql");
 
-const PORT = process.env.PORT || 4040;
-
 const app = express();
+const PORT = process.env.PORT || 4040;
+const RootSchema = require("./schema/schema");
+
+const cors = require("cors");
 
 app.use(cors());
 
 app.use(
   "/",
-  graphqlHTTP({
+  graphqlHTTP(() => ({
     schema: RootSchema,
     graphiql: true,
-  })
+  }))
 );
 
 app.listen(PORT, () => console.log("GQL server live"));
