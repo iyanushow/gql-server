db87cceb86f81d9a6638847b0e110efcf1e24a62
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-07-14 11:05:21 +0100

    another cors trial

--- index.js
+++ index.js
@@ -7,14 +7,35 @@ const RootSchema = require("./schema/schema");
 
 const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
+
+// app.use(
+//   "/",
+//   graphqlHTTP(() => ({
+//     schema: RootSchema,
+//     graphiql: true,
+//   }))
+// );
+
+app.use("/gql", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(
-  "/",
-  graphqlHTTP(() => ({
+  "/gql",
+  graphqlHTTP({
     schema: RootSchema,
     graphiql: true,
-  }))
+  })
 );
 
 app.listen(PORT, () => console.log("GQL server live"));
