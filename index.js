345a705f3e68c06f011ef14baf26af074c880e10
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-07-14 08:44:05 +0100

    test

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const PORT = process.env.PORT || 4040;
 const app = express();
 
 const corsOptions = {
-  origin: "https://shady-heli.netlify.app/",
+  origin: "https://shady-heli.netlify.app",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
 // app.options("*", cors());
 
-app.use(
-  "/gql",
-  graphqlHTTP({
-    schema: RootSchema,
-    graphiql: true,
-  })
-);
+// app.use(
+//   "/gql",
+//   graphqlHTTP({
+//     schema: RootSchema,
+//     graphiql: true,
+//   })
+// );
 
 app.get("/", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for an allowed domain." });
